fdd74851555806a72fcae50ddf1b3fa041cd8caf
newsonesnsd <newsonesnsd@gmail.com>
2017-04-18 19:29:20 +0700

    change hostname

--- src/publicregistergroup/controller/ConnectBuilder.java
+++ src/publicregistergroup/controller/ConnectBuilder.java
@@ -1,11 +1,12 @@
 package publicregistergroup.controller;
 import java.sql.*;
+
 public class ConnectBuilder {
     public void getConnection(){
         String db_name = "publicRegis";
         String user = "public-2";
         String pass = "public";
-        String hostName = "10.4.56.8";
+        String hostName = "10.4.56.8:3306";
         String driverName = "com.mysql.jdbc.Driver";
         try{
             Class.forName(driverName);
@@ -14,10 +15,10 @@ public class ConnectBuilder {
             System.out.println("ConnectComplete");
         }
         catch (Exception e){
-            System.out.println(e.getMessage());
+            System.out.println(e);
         }  
     }
     public static void main(String[] args){
-          new ConnectBuilder().connection();
+          new ConnectBuilder().getConnection();
     }
 }
