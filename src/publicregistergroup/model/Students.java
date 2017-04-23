a45f02dd2cd08c5146fca43cebae4bbf7071b133
newsonesnsd <newsonesnsd@gmail.com>
2017-04-23 21:45:22 +0700

    add contructer of student but Picture I don't know what type so i commented

--- src/publicregistergroup/model/Students.java
+++ src/publicregistergroup/model/Students.java
@@ -6,12 +6,25 @@
 package publicregistergroup.model;
 
 import java.sql.Connection;
+//import java.sql.Date;
+import java.util.Date;
 import publicregistergroup.controller.*;
 /**
  *
  * @author newsone
- * This is Class for student in databse 
+ * This is Class for student in database not ldap 5555
  */
 public class Students {
     static Connection con = ConnectionBuilder.getConnection();
+    private long std_id;
+    private String std_name;
+    private String std_faculty;
+    private String std_department;
+    private String std_major;
+    private String std_tel;
+    //private bla bla std_picture;
+    private String std_role;
+    private Date created_at;
+    private Date updated_at;
+    
 }
