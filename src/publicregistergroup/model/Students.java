d7ef457ae28ce1ac5dd018f57527de8ea632a885
newsonesnsd <newsonesnsd@gmail.com>
2017-04-23 22:07:15 +0700

    add get, set method (auto from netbean) - I have no idea now, in midnight i think i will have some idea

--- src/publicregistergroup/model/Students.java
+++ src/publicregistergroup/model/Students.java
@@ -26,5 +26,87 @@ public class Students {
     private String std_role;
     private Date created_at;
     private Date updated_at;
+
+    public static Connection getCon() {
+        return con;
+    }
+
+    public static void setCon(Connection con) {
+        Students.con = con;
+    }
+
+    public long getStd_id() {
+        return std_id;
+    }
+
+    public void setStd_id(long std_id) {
+        this.std_id = std_id;
+    }
+
+    public String getStd_name() {
+        return std_name;
+    }
+
+    public void setStd_name(String std_name) {
+        this.std_name = std_name;
+    }
+
+    public String getStd_faculty() {
+        return std_faculty;
+    }
+
+    public void setStd_faculty(String std_faculty) {
+        this.std_faculty = std_faculty;
+    }
+
+    public String getStd_department() {
+        return std_department;
+    }
+
+    public void setStd_department(String std_department) {
+        this.std_department = std_department;
+    }
+
+    public String getStd_major() {
+        return std_major;
+    }
+
+    public void setStd_major(String std_major) {
+        this.std_major = std_major;
+    }
+
+    public String getStd_tel() {
+        return std_tel;
+    }
+
+    public void setStd_tel(String std_tel) {
+        this.std_tel = std_tel;
+    }
+
+    public String getStd_role() {
+        return std_role;
+    }
+
+    public void setStd_role(String std_role) {
+        this.std_role = std_role;
+    }
+
+    public Date getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(Date created_at) {
+        this.created_at = created_at;
+    }
+
+    public Date getUpdated_at() {
+        return updated_at;
+    }
+
+    public void setUpdated_at(Date updated_at) {
+        this.updated_at = updated_at;
+    }
+    
+    
     
 }
