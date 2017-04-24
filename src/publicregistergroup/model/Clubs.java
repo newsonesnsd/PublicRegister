869dbe2a30ccf0e101b7396eddd580dd05e0ace6
newsonesnsd <newsonesnsd@gmail.com>
2017-04-24 12:50:52 +0700

    add getter, setter method (Class Club)

--- src/publicregistergroup/model/Clubs.java
+++ src/publicregistergroup/model/Clubs.java
@@ -18,7 +18,28 @@ public class Clubs {
     private String club_name;
     private String club_description;
 
+    public int getClub_id() {
+        return club_id;
+    }
 
-    
+    public void setClub_id(int club_id) {
+        this.club_id = club_id;
+    }
+
+    public String getClub_name() {
+        return club_name;
+    }
+
+    public void setClub_name(String club_name) {
+        this.club_name = club_name;
+    }
+
+    public String getClub_description() {
+        return club_description;
+    }
+
+    public void setClub_description(String club_description) {
+        this.club_description = club_description;
+    }
     
 }
