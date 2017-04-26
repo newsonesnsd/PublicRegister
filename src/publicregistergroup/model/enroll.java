d150a2392a5d8e862c5e8f8a8d4c66847635c863
newsonesnsd <newsonesnsd@gmail.com>
2017-04-26 08:52:26 +0700

    add Class enroll Prepare for Database

---
+++ src/publicregistergroup/model/enroll.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package publicregistergroup.model;
+
+import java.sql.Connection;
+import publicregistergroup.controller.*;
+/**
+ *
+ * @author newso
+ */
+public class enroll {
+    static Connection con = ConnectionBuilder.getConnection();
+    /*
+    Wait for database 
+    
+    */
+}
