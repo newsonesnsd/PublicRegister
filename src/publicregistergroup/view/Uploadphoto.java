40e4b4f0dddc7fd54d091ba8f582438a2b7cf075
newsonesnsd <newsonesnsd@gmail.com>
2017-04-27 22:05:37 +0700

    put ; after import *-* (I miss ; 5555)

--- src/publicregistergroup/view/Uploadphoto.java
+++ src/publicregistergroup/view/Uploadphoto.java
@@ -9,7 +9,7 @@ import java.awt.Image;
 import java.io.File;
 import javax.swing.ImageIcon;
 import javax.swing.JFileChooser;
-import javax.swing.JFrame
+import javax.swing.JFrame;
 import javax.swing.filechooser.FileNameExtensionFilter;
 import publicregistergroup.controller.ConnectionBuilder;
 
