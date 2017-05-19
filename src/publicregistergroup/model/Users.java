b72e1f7f91f1a042bcb8e9f60a04d8ee874c5287
newsonesnsd <newsonesnsd@gmail.com>
2017-05-19 14:25:45 +0700

    CHANGE Users Class

--- src/publicregistergroup/model/Users.java
+++ src/publicregistergroup/model/Users.java
@@ -14,7 +14,7 @@ import publicregistergroup.controller.LdapAuthen;
  *
  * @author newso
  */
-public class User {
+public class Users {
     static Connection con = ConnectionBuilder.getConnection();
     private int user_id;
     private long username;
@@ -58,8 +58,8 @@ public class User {
         this.update_at = update_at;
     }
     
-    public static User getUsers(long username) {
-        User user = new User();
+    public static Users getUsers(long username) {
+        Users user = new Users();
         long std = 0;
         /*String query = "SELECT a.std_id as username, a.std_name, b.password "
                 + "FROM students AS a LEFT OUTER JOIN user as b ON a.std_id = b.username "
