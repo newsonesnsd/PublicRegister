9c9700f3ed0fe35c8411521dbbd96c2a1db3ef6e
newsonesnsd <newsonesnsd@gmail.com>
2017-05-20 09:34:34 +0700

    Enroll Stacking @Enroll

--- src/publicregistergroup/model/Enroll.java
+++ src/publicregistergroup/model/Enroll.java
@@ -25,8 +25,8 @@ import publicregistergroup.view.Login;
 public class Enroll {
     static Connection con = ConnectionBuilder.getConnection();
     private static int enroll_id;
-    private static int club_id;
-    private static long std_id = Login.getStdId();
+    private static int club_id = 48;
+    private static long std_id = 59130500004L;//Login.getStdId();
     private static Date create_at;
     private static Calendar update_at = Calendar.getInstance();
     private static int countId = getCountId();
@@ -44,19 +44,19 @@ public class Enroll {
             System.out.println(ex + "\n" + ex.getMessage());
             ex.printStackTrace();
         }
-        return countId;
+        return countId+1;
     }
     
     public static void enrollClub() {
-        int enrollId = countId;
         try {
             String sql = "INSERT INTO enroll (enroll_id, club_id, std_id, create_at, update_at) VALUES (?, ?, ?, ?, ?);";
             PreparedStatement pre = con.prepareStatement(sql);
-            pre.setInt(1, enrollId);
-            pre.setInt(1, club_id);
+            pre.setInt(1, getCountId());
+            pre.setInt(2, club_id);
             pre.setLong(3, std_id);
             pre.setDate(4, new java.sql.Date(new Date().getTime()));
             pre.setDate(5, new java.sql.Date(new Date().getTime()));
+            pre.executeUpdate();
             
         } 
         catch (SQLException ex) {
@@ -66,5 +66,7 @@ public class Enroll {
         
     }
     
-
+    public static void main(String[] args) {
+        enrollClub();
+    }
 }
